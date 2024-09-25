a84f06895833ed331f3a85f2bb9728c12eb5ecdf
Disha-69 <jannathossaindisha@gmail.com>
2024-09-25 21:57:39 +0600

    ok

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,5 @@
 document.getElementById("home").addEventListener('click',function(event){
     event.preventDefault();
 
-    window.location.href='/index.html';
+    window.location.href='./index.html';
 })
